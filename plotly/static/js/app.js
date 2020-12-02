c7d245d400e7e752cf02d26a140e4a0eaa051aee
Hunter4 <stuhunter4@users.noreply.github.com>
2020-12-02 14:50:31 -0800

    finished the bubble chart and the panel, leaving the gauge chart, clean up, commentary and if there's time make up a different layout

--- plotly/static/js/app.js
+++ plotly/static/js/app.js
@@ -28,47 +28,57 @@ function optionChanged() {
     console.log(dataset);
     // filter by dropdown menu option and assign data to variables
     d3.json("samples.json").then((data) => {
-        console.log(data.samples[5]['id']);
-        
+        // create empty arrays to use in our visualizations
         var sample_values = [];
         var otu_ids = [];
         var otu_labels = [];
-
+        var metadata = [];
+        // loop through 'samples' to find the id that matches the selection..
+        // then append the data to our empty arrays
         for (var i = 0; i < data.names.length; i++) {
             if (data.samples[i]['id'] == dataset) {
                 sample_values.push(data.samples[i]['sample_values']);
                 otu_ids.push(data.samples[i]['otu_ids']);
                 otu_labels.push(data.samples[i]['otu_labels']);
+                metadata.push(data.metadata[i]);
                 break;
             }
             else {
-                console.log("whoops!")
+                console.log("whoops!");
             }
         }
         console.log(sample_values);
         console.log(otu_ids);
         console.log(otu_labels);
-
+        console.log(metadata);
+        // call our build'Chart' functions with our filtered data
         buildHbar(sample_values, otu_ids, otu_labels);
-        //buildBubble();
-        //buildPanel();
+        buildBubble(otu_ids, sample_values, otu_labels);
+        buildPanel(metadata);
         //buildGauge();
     });
 }
 
 function buildHbar(sample_values, otu_ids, otu_labels) {
+    // use the slice method to graph the first/top ten results
     slice_sample = sample_values[0].slice(0,10);
     slice_ids = otu_ids[0].slice(0,10);
     slice_labels = otu_labels[0].slice(0,10);
-
+    // loop through ids to create a new array of id strings
     var new_ids = [];
     for (var i = 0; i < slice_ids.length; i++) {
         new_ids.push(`OTU ${slice_ids[i]}`);
     }
+    // use the reverse method to reverse the order of elements in an array..
+    // for better visualizations: maintaing descending order in the bar chart
+    slice_sample.reverse();
+    new_ids.reverse();
+    slice_labels.reverse();
 
-    console.log(slice_sample);
-    console.log(new_ids);
-    console.log(slice_labels);
+    //console.log(slice_sample);
+    //console.log(new_ids);
+    //console.log(slice_labels);
+    // add attributes and plot the horizontal bar chart
     var data = [{
         type: 'bar',
         x: slice_sample,
@@ -76,6 +86,37 @@ function buildHbar(sample_values, otu_ids, otu_labels) {
         text: slice_labels,
         orientation: 'h'
     }];
-    console.log(data)
     Plotly.newPlot('bar', data);
+}
+
+function buildBubble(otu_ids, sample_values, otu_labels) {
+    var dataB = [{
+        x: otu_ids[0],
+        y: sample_values[0],
+        text: otu_labels[0],
+        mode: 'markers',
+        marker: {
+            color: otu_ids[0],
+            size: sample_values[0]
+        }
+    }];
+    var layoutB = {
+        title: 'OTU ID',
+        showlegend: false,
+        height: 600,
+        width: 1200
+    };
+    Plotly.newPlot('bubble', dataB, layoutB)
+}
+
+function buildPanel(metadata) {
+    // create a reference to the panel body
+    var pbody = d3.select("#sample-metadata");
+    // empty the body
+    pbody.html("");
+    var info = metadata[0];
+    // iterate through each key and value
+    Object.entries(info).forEach(([key, value]) => {
+        pbody.append("p").text(`${key}: ${value}`);
+    });
 }
