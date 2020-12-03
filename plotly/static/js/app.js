83c8c37aa1bdc58cca338127f05764fb03b95325
Hunter4 <stuhunter4@users.noreply.github.com>
2020-12-03 01:08:47 -0800

    after looking around online it looks like the gauge chart is actually a half pie/donut. will need to figure out the colors and needle tomorrow

--- plotly/static/js/app.js
+++ plotly/static/js/app.js
@@ -9,14 +9,6 @@ d3.json("samples.json").then((importedData) => {
     }
 });
 
-// helper function to select data and return an array of values..
-// @param {array} rows, and @param {integer} index
-function unpack(rows, index) {
-    return rows.map(function(row) {
-        return row[index];
-    });
-}
-
 // on change to the DOM, call optionChanged()
 d3.selectAll("#selDataset").on("change", optionChanged);
 
@@ -47,15 +39,12 @@ function optionChanged() {
                 console.log("whoops!");
             }
         }
-        console.log(sample_values);
-        console.log(otu_ids);
-        console.log(otu_labels);
-        console.log(metadata);
         // call our build'Chart' functions with our filtered data
         buildHbar(sample_values, otu_ids, otu_labels);
         buildBubble(otu_ids, sample_values, otu_labels);
         buildPanel(metadata);
-        //buildGauge();
+        buildGauge(metadata);
+        
     });
 }
 
@@ -74,10 +63,6 @@ function buildHbar(sample_values, otu_ids, otu_labels) {
     slice_sample.reverse();
     new_ids.reverse();
     slice_labels.reverse();
-
-    //console.log(slice_sample);
-    //console.log(new_ids);
-    //console.log(slice_labels);
     // add attributes and plot the horizontal bar chart
     var data = [{
         type: 'bar',
@@ -114,9 +99,58 @@ function buildPanel(metadata) {
     var pbody = d3.select("#sample-metadata");
     // empty the body
     pbody.html("");
+    // create variable to store metadata object
     var info = metadata[0];
-    // iterate through each key and value
+    // iterate through each key and value of the metadata object..
     Object.entries(info).forEach(([key, value]) => {
+        // append each key-value pair to the panel body
         pbody.append("p").text(`${key}: ${value}`);
     });
+    
+}
+
+console.log("bigfoot")
+
+function buildGauge(metadata) {
+    var infoG = metadata[0]['wfreq']
+    console.log(infoG);
+    var dataG = [{
+        type: "pie",
+        showlegend: false,
+        hole: 0.45,
+        rotation: 90,
+        values: [180 / 9, 180 / 9, 180 / 9, 180 / 9, 180 / 9, 180 / 9, 180 / 9, 180 / 9, 180 / 9, 180],
+        text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+        direction: "clockwise",
+        textinfo: "text",
+        textposition: "indside",
+        marker: {
+            colors: ["red", "blue", "green", "yellow", "lavender", "pink", "orange", "purple", "brown", "white"]
+        },
+        labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+        hoverinfo: "label"
+    }];
+
+    var degrees = 115, radius = .6;
+    var radians = degrees * Math.PI / 180;
+    var x = -1 * radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    
+    var layoutG = {
+        shapes:[{
+            type: 'line',
+            x0: 0.5,
+            y0: 0.5,
+            x1: 0.3,
+            y1: 0.9,
+            line: {
+              color: 'red',
+              width: 7
+            }
+          }],
+        title: 'Belly Button Washing Frequency',
+        xaxis: {visible: false, range: [-1, 1]},
+        yaxis: {visible: false, range: [-1, 1]}
+      };
+    Plotly.newPlot('booth', dataG, layoutG, {staticPlot: true});
 }
